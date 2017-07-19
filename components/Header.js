95c0e22c430b58b3df0aba90d051a580e3a3635d
林彦君 <josh.lin@pisofttech.com>
2017-07-19 20:21:18 +0800

    basic

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,18 @@
 
 import React from 'react'
 import { translate } from 'react-i18next'
-import getNavigation from 'next-navigation'
 import { connect } from 'react-redux'
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
 
-import { default as routes } from '../tools/routes'
 import i18nHelper from '../tools/i18n-helper'
-import { getUser, logout} from '../tools/store/user'
+import { getUser, logout } from '../tools/store/user'
 import { getContextedFetch } from '../tools/fetch'
+import { Link } from '../tools/routes'
 
-const MyNav = getNavigation(routes)
+const langNames = {
+  en: 'English',
+  zh: '中文'
+}
 
 class Header extends React.Component {
 
@@ -17,79 +20,59 @@ class Header extends React.Component {
     i18nHelper.setCurrentLanguage(e.target.value)
   }
 
-  handleLogout(){
-    var {dispatch} = this.props
+  handleLogout() {
+    var { dispatch } = this.props
     dispatch(logout())
   }
 
   render() {
-    const { url, t, user } = this.props
+    const { t, user } = this.props
 
-    var tprops = {
-      ulProps: {
-        className: 'mynav',
-      },
-      links: this.getLinks(t),
-      activeStyle: {},
-      activeClassName: 'on',
-      url,
-    }
-    return (<div className="nav-wrap">
-      <MyNav {...tprops} />
-      <p>{JSON.stringify(user)}</p>
-      <style jsx>{`
-          .nav-wrap :global(.mynav) {
-            border: 1px solid black;
-          }
+    var login = (user && user.username) ? (
+      <NavItem onClick={this.handleLogout.bind(this)}>{t('Logout')}</NavItem>
+    ) : (
+        <Link route="login">
+          <NavItem href="#">{t('Login')}</NavItem>
+        </Link>
+      )
 
-          .nav-wrap :global(.mynav .on a) {
-            font-weight: bold;
-          }
-        `}</style>
-    </div>
+    return (<Navbar inverse collapseOnSelect>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="#">next.js-boilerplate</a>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav>
+          <Link route="index">
+            <NavItem href="#">{t('Home')}</NavItem>
+          </Link>
+          <Link route="about">
+            <NavItem href="#">{t('About')}</NavItem>
+          </Link>
+          <Link route="posts">
+            <NavItem href="#">{t('Posts')}</NavItem>
+          </Link>
+          {login}
+        </Nav>
+        <Nav pullRight style={{ marginRight: 0 }}>
+          <NavDropdown title={langNames[i18nHelper.getCurrentLanguage()]} id="basic-nav-dropdown">
+            <MenuItem onClick={() => i18nHelper.setCurrentLanguage('en')}>{langNames.en}</MenuItem>
+            <MenuItem onClick={() => i18nHelper.setCurrentLanguage('zh')}>{langNames.zh}</MenuItem>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
     )
   }
-
-  getLinks(t) {
-    var that = this
-    var { user} = this.props
-    return [{
-      linkProps: { route: "index" },
-      children: <a >{t('Home')}</a>,
-    }, {
-      linkProps: { route: "about" },
-      children: <a >{t('About')}</a>,
-      activeStyle: { color: 'blue', },
-    }, {
-      linkProps: { route: "posts" },
-      children: <a >{t('Posts')}</a>,
-      checkIsActive: ({ pathname }) => {
-        return ('/post' === pathname) || ('/posts' === pathname)
-      }
-    },
-    (user && user.username) ? {
-      nolink: true,
-      children: <a onClick={this.handleLogout.bind(this)}>{t('Logout')}</a>,
-    } : {
-        linkProps: { route: "login" },
-        children: <a >{t('Login')}</a>,
-      }, {
-      nolink: true,
-      children: (<select value={i18nHelper.getCurrentLanguage()} onChange={that.handleChangeLanguage.bind(that)}>
-        <option value="en">English</option>
-        <option value="zh">中文</option>
-      </select>),
-    }]
-  }
-
-
 
   static translateNS = ['common']
   static async getInitialProps(ctx) {
     const { store } = ctx
-    return await store.dispatch(getUser(getContextedFetch(ctx),store))
-    .catch(()=>{
-    })
+    return await store.dispatch(getUser(getContextedFetch(ctx), store))
+      .catch(() => {
+      })
   }
 }
 
--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,7 @@ import wrapper from '../tools/wrapper'
 
 const layout = (Page) => wrapper(class Layout extends React.Component {
   render() {
-    return (<div className="wrapper">
+    return (<div className="container">
       <Head>
         <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
